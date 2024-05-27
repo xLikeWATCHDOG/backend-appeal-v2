215df4c7aee1964c86513ce5136ed35ac21ddbcb
花花 <73741466+xLikeWATCHDOG@users.noreply.github.com>
2024-05-27 16:47:44 +0800

    init project.

---
+++ src/main/java/cn/watchdog/v2/appeal/BackendAppealV2Application.java
@@ -0,0 +1,13 @@
+package cn.watchdog.v2.appeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAppealV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAppealV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/cn/watchdog/v2/appeal/BackendAppealV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.watchdog.v2.appeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppealV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
